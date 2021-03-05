6539dfa1f11569d05e4cfe6860cb5db41acfa882
panos <panagiotis8panagiotopoulos@gmail.com>
2021-03-05 10:57:14 +0200

    renaming db

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ export default {
   port: process.env.PORT || 1234,
   ip: process.env.HOST || '0.0.0.0',
   mongo: {
-    uri: process.env.MONGO_URL || 'mongodb://localhost:27017/movierama'
+    uri: process.env.MONGO_URL || 'mongodb://localhost:27017/movierama-panagiop-db'
   },
   jwtSecret: process.env.JWT_SECRET || 'p@£%Tg#@!S56ds'
 };
